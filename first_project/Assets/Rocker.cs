f5ba78e61ec9ef9af548ad25e6a81d1bf544ba45
kskrypnyk <skrypnyk.kostiantyn@gmail.com>
2018-12-26 19:43:38 +0100

    Added sound to the rocket

--- first_project/Assets/Rocker.cs
+++ first_project/Assets/Rocker.cs
@@ -7,11 +7,13 @@ public class Rocker : MonoBehaviour
 {
 
     Rigidbody rigidbody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,8 +24,18 @@ public class Rocker : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space)) { rigidbody.AddRelativeForce(Vector3.up); }
-        if (Input.GetKey(KeyCode.A)) { rigidbody.AddRelativeForce(Vector3.left); }
-        if (Input.GetKey(KeyCode.D)) { rigidbody.AddRelativeForce(Vector3.right); }
+        if (Input.GetKey(KeyCode.Space)) { 
+            rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A)) { transform.Rotate(Vector3.forward); }
+        if (Input.GetKey(KeyCode.D)) { transform.Rotate(-Vector3.forward); }
     }
 }
