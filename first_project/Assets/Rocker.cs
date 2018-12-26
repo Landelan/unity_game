26794e74775332b8cc234339d74318f7e38e04bb
kskrypnyk <skrypnyk.kostiantyn@gmail.com>
2018-12-26 17:11:18 +0100

    Added keys to motions

---
+++ first_project/Assets/Rocker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocker : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) { rigidbody.AddRelativeForce(Vector3.up); }
+        if (Input.GetKey(KeyCode.A)) { rigidbody.AddRelativeForce(Vector3.left); }
+        if (Input.GetKey(KeyCode.D)) { rigidbody.AddRelativeForce(Vector3.right); }
+    }
+}
