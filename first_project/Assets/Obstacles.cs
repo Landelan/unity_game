5e97d2308cc1801ce4607509f16c7fff9dab9aa8
kskrypnyk <skrypnyk.kostiantyn@gmail.com>
2019-01-04 17:43:33 +0100

    Automate the movement of platform on second level

--- first_project/Assets/Obstacles.cs
+++ first_project/Assets/Obstacles.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Obstacles : MonoBehaviour
 {
 
-    [SerializeField] Vector3 movementVector;
-
+    [SerializeField] Vector3 movementVector = new Vector3(0f, 5f, 0f);
+    [SerializeField] float period = 2f;
 
     // 13 - 8
-    [SerializeField] [Range(0,1)] float movementRagne;
+    // SerializeField] [Range(0,1)] float movementRagne;
 
+    float movementRagne;
     Vector3 startingPosition;
 
     // Start is called before the first frame update
@@ -22,6 +23,14 @@ public class Obstacles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period == 0) { return;  }
+        float time = Time.time / 2;
+        float cycles = time / period;
+        const float PI = Mathf.PI * 2;
+        float SinWave = Mathf.Sin(cycles * PI);
+
+        movementRagne = SinWave;
+
         Vector3 offset = movementVector * movementRagne;
         transform.position = startingPosition - offset;
     }
