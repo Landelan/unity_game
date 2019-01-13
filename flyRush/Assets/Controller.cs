5b012fca6aea647dccf0779ad0976f6466d23316
kskrypnyk <skrypnyk.kostiantyn@gmail.com>
2019-01-13 10:58:42 +0100

    Added menu scene and music on it

---
+++ flyRush/Assets/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("LoadNextScene", 5f);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
