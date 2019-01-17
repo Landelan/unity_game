c41749c3abec09add701578c079fbe5cf21d3572
kskrypnyk <skrypnyk.kostiantyn@gmail.com>
2019-01-17 20:53:31 +0100

    Added few changes

---
+++ flyRush/Assets/ControllerShip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandartAssets.CrossPlatformInput;
+
+public class ControllerShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        float horizontalThrow = CrossPlatformInput.GetAxis("Horizontal");
+
+
+    }
+}
